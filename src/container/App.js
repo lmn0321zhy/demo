2194b4380ee2bade0c3e892d77f377991bfb9972
lmn0321zhy <liumengnan2013@163.com>
2018-07-17 23:36:38 +0800

    demo

--- src/container/App.js
+++ src/container/App.js
@@ -1,19 +1,22 @@
 
 import React from 'react';
-// import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import AjaxDemo from 'container/dashboard'
-// import Login from 'container/login'
+import Login from 'container/login'
+import Dashboard from 'container/dashboard'
+import Tabs from 'components/tabs'
+
+import styles from './App.less'
 
 export default class APP extends React.Component {
 	render() {
 		return (
 			<Router>
-				<div>
+				<div className={styles.app}>
 					<Switch>
-						<Route exact path="/" component={AjaxDemo} />
-						{/* <Route exact path="/" component={Login} /> */}
+						<Route exact path="/" component={Tabs} />
+						<Route exact path="/dashboard" component={Dashboard} />
 					</Switch>
 				</div>
 			</Router>
